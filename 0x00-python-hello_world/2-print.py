b996ffbf0fe754cae054984beedccc6107dbdebf
moryzulu <morymacho6@gmail.com>
2022-09-20 07:08:22 -0700

    print my first string in c

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-import pycodestyle
-print('\"programming is like building a multilingual puzzle')
+    import pycodestyle
+    print('\"programming is like building a multilingual puzzle')
