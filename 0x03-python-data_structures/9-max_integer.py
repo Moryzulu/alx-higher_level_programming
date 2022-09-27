15f4ac7e27c47af11c8c14de619688172e67e1e1
moryzulu <morymacho6@gmail.com>
2022-09-27 00:33:34 -0700

    data structures

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,6 +6,6 @@ def max_integer(my_list=[]):
     bigger_num = min(my_list)
     for i in my_list:
         if i > bigger_num:
-           bigger_num = i
+            bigger_num = i
 
     return bigger_num
